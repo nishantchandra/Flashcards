55f069c53a9a8a88618169193ac553dee8398676
dbc08 <apprentice@dbc08.local>
2013-09-06 16:25:54 -0700

    finished

--- controller.rb
+++ controller.rb
@@ -1,24 +1,35 @@
-model = Model.new(text_file)
-model.gather_data
-##model has knowledge of a list of cards
- 
-def check_one_card(one_card, view)
-  view.show_card(one_card) 
-  view.get_response
-  view.check_response
-  string = view.output_result
-  return string if string == "Correct" 
-  check_one_card(one_card, view)
+require "./Model.rb"
+require "./view.rb"
+
+class Controller
+  attr_reader :model, :view
+  
+  def initialize(model, view)
+    @model = model
+    @view = view
+  end
+
+  def check_one_card(one_card)
+    view.show_card(one_card) 
+    view.get_response
+    view.check_response
+    string = view.output_result
+    return string if string == "Correct" 
+    check_one_card(one_card)
+  end
+
+  def begin_studying
+  model.create_flashcards
+  i = 0
+    until i == model.flashcards.length
+      check_one_card(model.flashcards[i])
+      i += 1
+    end
+  end
 end
 
-loop over card_id, adding one to the iteration each time
-  one_card = model.get_card(card_id) (should return a hash)
-  check_one_card(one_card) 
-end
+new_view = View.new
+new_model = Model.new('flashcard_samples.txt')
+new_controller = Controller.new(new_model,new_view)
+new_controller.begin_studying
 
-
-card_id = integer
-one_card = hash 
-values in hash = definition is a= string , answer is a  string, card_id is a integer
-
-flashcard_samples.txt
